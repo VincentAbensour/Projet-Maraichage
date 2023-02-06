ac69d1eb5bba86fcb606b1bd0f1c42795506667f
Vincent <vincentabensour@yahoo.fr>
2023-02-06 12:09:39 +0100

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -138,9 +138,10 @@ AUTH_USER_MODEL = "account.Account"
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.laposte.net'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'vincentabensour@laposte.net'
-EMAIL_HOST_PASSWORD = "VincentLaposte232!!"
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ""
 EMAIL_USE_TLS = True
 
 # Url de base pour se logger
 LOGIN_URL = "/compte/login"
+
